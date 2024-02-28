2a77d2ab35379292b7a79f0c5df20b19f6f45443
chancehl <clinz.dev@gmail.com>
2024-02-28 00:10:50 -0800

    optimizes toeplitz matrix problem

--- src/main/java/problems/easy/ToeplitzMatrix.java
+++ src/main/java/problems/easy/ToeplitzMatrix.java
@@ -1,43 +1,65 @@
 package problems.easy;
 
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+/**
+ * Complexity: O(M x N)
+ *
+ * Ironically, the unoptimized brute force solution was considerably faster because of the overhead
+ * of managing the List class in Java.
+ */
 public class ToeplitzMatrix {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int column = 0;
+        List<List<Integer>> diagonals = this.generateDiagonals(matrix);
 
-        while (column < matrix[0].length) {
-            int row = 0;
-
-            while (row < matrix.length) {
-                boolean hasSingleValue = this.doesDiagonalHaveSingleValue(row, column, matrix);
-
-                if (!hasSingleValue) {
+        for (List<Integer> diagonal : diagonals) {
+            for (int i : diagonal) {
+                if (i != diagonal.getFirst()) {
                     return false;
                 }
-
-                row++;
             }
-
-            column++;
         }
 
         return true;
     }
 
-    private boolean doesDiagonalHaveSingleValue(int startingRow, int startingColumn, int[][] matrix) {
-        int row = startingRow;
-        int col = startingColumn;
+    private List<List<Integer>> generateDiagonals(int[][] matrix) {
+        List<List<Integer>> diagonals = new ArrayList<>();
 
-        int value = matrix[row][col];
+        Set<Point> visited = new HashSet<>();
 
-        while (row < matrix.length && col < matrix[0].length) {
-            if (matrix[row][col] != value) {
-                return false;
+        int startingRow = 0;
+
+        while (startingRow < matrix.length) {
+            int startingCol = 0;
+
+            while (startingCol < matrix[0].length) {
+                if (!visited.contains(new Point(startingRow, startingCol))) {
+                    int i = startingRow;
+                    int j = startingCol;
+
+                    List<Integer> diagonal = new ArrayList<>();
+
+                    while (i < matrix.length && j < matrix[0].length) {
+                        diagonal.add(matrix[i][j]);
+
+                        visited.add(new Point(i, j));
+
+                        i++;
+                        j++;
+                    }
+
+                    diagonals.add(diagonal);
+                }
+
+                startingCol++;
             }
 
-            row++;
-            col++;
+            startingRow++;
         }
 
-        return true;
+        return diagonals;
     }
 }
